91e3a9bd0ac46ca4b1157f747f81a7436045820d
izanami <120745634+IzanamiiDevv@users.noreply.github.com>
2024-06-27 02:32:13 +0800

    Update lexer.h

--- source/includes/lexer.h
+++ source/includes/lexer.h
@@ -3,3 +3,70 @@
 #include <iostream>
 #include <vector>
 #include <string>
+
+namespace Lexer {
+    enum Type {
+        Number,
+        String,
+        Boolean,
+        Keywords,
+        Constant,
+        Identifier,
+        Operator,
+        Equal,
+        LeftBrace,
+        RightBrace,
+    };
+
+    struct Token {
+        std::string value;
+        Type type;
+    };
+
+    Token createToken(std::string value, Type type) {
+        Token buffer;
+        buffer.value = value;
+        buffer.type = type;
+        return buffer;
+    }
+
+    bool tokenizedSingleChars(std::vector<Token> *tokens, char c) {
+        switch (c) {
+            case '+' : case '-' : case '*' : case '/': {
+                std::string temp(1, c);
+                (*tokens).push_back(createToken(temp, Type::Operator));
+                return true;
+            }
+
+            case '=' : {
+                std::string temp(1, c);
+                (*tokens).push_back(createToken(temp, Type::Equal));
+                return true;
+            }
+
+            case '(' : case '{' : case '[' : {
+                std::string temp(1, c);
+                (*tokens).push_back(createToken(temp, Type::LeftBrace));
+                return true;
+            }
+
+            case ')' : case '}' : case ']' : {
+                std::string temp(1, c);
+                (*tokens).push_back(createToken(temp, Type::LeftBrace));
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    std::vector<Token> tokenized(std::string source) {
+        std::vector<Token> tokens;
+
+        for(char c : source ) {
+            if(tokenizedSingleChars(&tokens, c)) continue;
+        }
+
+        return tokens;
+    }
+}
